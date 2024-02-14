7fce8257f2fb71d47cd37f129f1e32f4450e03e1
Geysiane <rgeisinha@gmail.com>
2024-02-14 20:24:36 -0300

    Shopping-Cart

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+
+import propTypes from 'prop-types';
+
+import './ProductCard.css';
+
+import { BsFillCartPlusFill } from 'react-icons/bs';
+import formatCurrency from '../../utils/formatCurrency';
+import AppContext from '../../context/AppContext';
+
+function ProductCard({ data }) {
+
+  const { title, thumbnail, price } = data;
+
+  const { cartItens, setCartItens } = useContext(AppContext);
+
+  const handleAddCart = () => setCartItens ([ ...cartItens, data ]);
+    
+  
+
+  return (
+    <section className="product-card">
+
+      <img src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
+        alt="product" 
+        className="card__image" />
+
+      <div className="card__infos">
+        <h2 className="card__price">{formatCurrency(price, 'BRL')}</h2>
+        <h2 className="card__title">{title}</h2>
+      </div>
+
+      <button
+        type="button" 
+        className="button__add-cart"
+        onClick={ handleAddCart}
+      >
+        <BsFillCartPlusFill />
+      </button>
+    </section>
+  );
+}
+
+export default ProductCard;
+
+ProductCard.propTypes = {
+  data: propTypes.shape({}),
+}.isRequired;
